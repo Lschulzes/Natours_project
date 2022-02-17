bd35ca88080319e4f077eeba5b0a6174d06e0843
lschulzes <lucassilvaiee@gmail.com>
2022-02-17 14:08:29 -0300

    Add some model level validators

--- models/TourModel.ts
+++ models/TourModel.ts
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'Tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have less or equal than 10 characters'],
     },
     slug: {
       type: String,
@@ -16,6 +18,8 @@ const tourSchema = new mongoose.Schema(
     ratingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [1, 'Rating must be below 5.0'],
     },
     ratingQuantity: {
       type: Number,
@@ -33,6 +37,10 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'Tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium or difficulty',
+      },
     },
     duration: {
       type: Number,
