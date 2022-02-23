8946cc79c2e1ee8cdbca885dda5a59befb359b34
lschulzes <lucassilvaiee@gmail.com>
2022-02-23 20:40:20 -0300

    Add error handling for dev/prod

--- controllers/ErrorController.ts
+++ controllers/ErrorController.ts
@@ -1,6 +1,32 @@
-import { NextFunction, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Response } from 'express';
 import { RequestCustom } from '../custom_types';
 import { AppError } from './../resources/helpers';
+
+const sendErrorDev = (err: AppError, res: Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err: AppError, res: Response) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    return;
+  }
+
+  console.error(`Error ${err}`);
+
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went very wrong!',
+  });
+};
 
 export const errorHandler = (
   err: AppError,
@@ -10,8 +36,7 @@ export const errorHandler = (
 ) => {
   err.statusCode = err.statusCode ?? 500;
   err.status = err.status ?? 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  else sendErrorProd(err, res);
 };
