b74c0e81d080cc20a974c7426f40a61a2bd8ada8
lschulzes <lucassilvaiee@gmail.com>
2022-04-04 08:22:35 -0300

    Feat: Add overview tours page

--- controllers/ViewController.ts
+++ controllers/ViewController.ts
@@ -1,11 +1,23 @@
-export const getOverview = (_req, res) => {
+import { catchAsync } from './../resources/helpers';
+import { Request, Response } from 'express';
+import TourModel from '../models/TourModel';
+
+export const getOverview = catchAsync(async (_req: Request, res: Response) => {
+  const tours = await TourModel.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-export const getTour = (_req, res) => {
+export const getTour = catchAsync(async (req: Request, res: Response) => {
+  const { tourSlug } = req.params;
+
+  const [tour] = await TourModel.find({ slug: tourSlug });
+
   res.status(200).render('tour', {
     title: 'The Snow Explorerer',
+    tour,
   });
-};
+});
--- routes/ViewRoutes.ts
+++ routes/ViewRoutes.ts
@@ -1,10 +1,11 @@
-import { getOverview, getTour } from './../controllers/ViewController';
+import { getTour, getOverview } from './../controllers/ViewController';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', getOverview);
 
-router.get('/tour', getTour);
+router.get('/tours', getOverview);
+router.get('/tours/:tourSlug', getTour);
 
 export default router;
